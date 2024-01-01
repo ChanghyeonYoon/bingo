94cd5c352a31cb8f883c3d19430a45f6f31bee8b
Changhyeon Yoon <hyeon120411@gmail.com>
2024-01-02 00:11:57 +0900

    BINGO

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,64 @@
-import Image from 'next/image'
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import { QUESTIONS } from "@/constants";
+import gsap from "gsap";
 
 export default function Home() {
+  const [answerList, setAnswerList] = React.useState<Record<string, string>>({});
+  const handleSaveAnswer = (idx: number) => () => {
+    const answer = window?.prompt("정답을 입력해주세요.");
+    if (!answer) return;
+    setAnswerList((prev) => ({
+      ...prev,
+      [`Q-${idx}`]: answer,
+    }));
+
+    gsap.to(`#Q-${idx}`, {
+      duration: 1,
+      rotationY: 180,
+      transformStyle: "preserve-3d",
+      transformOrigin: "center",
+      ease: "power2.inOut",
+      backgroundColor: "#FF9900",
+    });
+
+    gsap.to(`#Q-${idx} div`, {
+      duration: 1,
+      rotationY: 180,
+      transformStyle: "preserve-3d",
+      transformOrigin: "center",
+      ease: "power2.inOut",
+    });
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main className='flex h-screen w-screen justify-center bg-white'>
+      <div className='flex h-full w-full max-w-[420px] flex-col items-center ring-1 ring-gray-200'>
+        <Image src={"/images/check.svg"} alt={"check"} width={420} height={50} />
+        <Image src={"/images/title.svg"} alt={"check"} width={420} height={50} />
+        <Image src={"/images/sub_title.svg"} alt={"check"} width={340} height={50} className='mb-10' />
+        <section className='grid grid-cols-4 gap-3'>
+          {QUESTIONS.map((question, idx) => {
+            const isAnswered = !!answerList?.[`Q-${idx}`];
+            return (
+              <div
+                id={`Q-${idx}`}
+                key={question}
+                onClick={handleSaveAnswer(idx)}
+                className='flex h-28 w-[90px] cursor-pointer items-center justify-center border-[2px] border-black bg-white shadow-2xl'
+              >
+                <div>
+                  <p className={`whitespace-pre-line text-center ${idx === 6 ? "text-xs" : "text-sm"}`}>
+                    {isAnswered ? answerList?.[`Q-${idx}`] : question}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </section>
       </div>
     </main>
-  )
+  );
 }
---
+++ prettier.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+    plugins: ["prettier-plugin-tailwindcss"],
+    singleQuote: false,
+    semi: true,
+    useTabs: false,
+    tabWidth: 2,
+    trailingComma: "all",
+    printWidth: 120,
+    arrowParens: "always",
+    jsxSingleQuote: true,
+    bracketSpacing: true,
+    bracketSameLine: false,
+};
